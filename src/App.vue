fb0563820536c66f21d485cd8ffbbd9689d338cc
MesoBolt <marius@kthomas.de>
2019-02-03 14:10:49 +0100

    Test

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DynamicalForm_1 msg="Welcome to Your Vue.js App"/>
+    <!-- <Field/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DynamicalForm_1 from './components/DynamicalForm_1.vue'
+// import Field from './components/Field.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    DynamicalForm_1
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/components/DynamicalForm_1.vue
+++ src/components/DynamicalForm_1.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="hello">
+  <!-- <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
@@ -27,32 +27,66 @@
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
+  </div> -->
+  <div class="container">
+    <button class="btn btn-success mt-5 mb-5"
+      @click="AddNewEmployeeForm">
+      New Employee
+    </button>
+
+    <div class="card mb-3" v-for="(employee, index) in employees">
+      <div class="card-body">
+        <span class="float-right" style="cursor:pointer"
+          @click="deleteEmployeeForm(index)">
+          X
+        </span>
+
+        <h4 class="card-title">Add Employee (index:{{ index }} )</h4>
+
+        <div class="employee-form">
+          <input type="text" class="form-control mb-2" placeholder="Name"
+            v-model="employee.name">
+
+          <input type="text" class="form-control mb-2" placeholder="Job"
+            v-model="employee.job">
+
+          <textarea cols="30" rows="10" placeholder="About"
+            v-model='employee.about'>></textarea>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  el: '.container',
+  data: function() {
+return {
+    employees: [
+      {
+        name: '',
+        job: '',
+        about: ''
+      }
+    ]
+  }
+  },
+  methods: {
+    AddNewEmployeeForm () {
+      this.employees.push({
+        name: '',
+        job: '',
+        about: ''
+      })
+    },
+    deleteEmployeeForm (index) {
+      this.employees.splice(index, 1)
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
