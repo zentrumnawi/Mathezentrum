882abcf174c3c2ca17c5d593e8e9b58cfdf35605
MesoBolt <marius@kthomas.de>
2019-03-08 13:44:42 +0100

    Darstellung verbessert

--- src/components/AttendeeForm.vue
+++ src/components/AttendeeForm.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="card mb">
       <h2>Teilnehmerliste</h2>
-        <div class="attendee form">
+        <div class="attendee_form">
           <tbody>
               <tr>
                 <td><input type="text" class="form-control mb-2" placeholder="Vorname, Nachname"
@@ -11,8 +11,11 @@
                 v-model="rows.matriculation"></td>
                 <td><input type="text" class="form-control mb-2" placeholder="Studienfach"
                 v-model="rows.faculty"></td>
-                <td><select class="form-control" v-model="rows.semester"><option v-for="semester in 10" :key="semester.id">{{semester}}</option></select></td>
-                <td><button class="btn btn-success mt-5 mb-5" v-on:click="AddNewStudentForm()">
+                <td><select class="form-control mb-2" v-model="semester_select">
+                  <option :value="null">Fachsemester</option>
+                  <option v-for="semester in 10" :key="semester.id">{{semester}}</option>
+                  </select></td>
+                <td><button class="btn btn-success mb-2" v-on:click="AddNewStudentForm()">
                   Save
                   </button>
                 </td>
@@ -29,13 +32,13 @@
               </tr>
             </thead>
             <tbody>
-              <tr v-for="(row, index) in attendees" :key="(row, index).id">
-                <th scope="row">{{index+1}}</th>
+              <tr class="attendee_rows" v-for="(row, index) in attendees" :key="(row, index).id">
+                <td scope="row">{{index+1}}</td>
                   <td>{{row.name}}</td>
                   <td>{{row.matriculation}}</td>
                   <td>{{row.faculty}}</td>
                   <td>{{row.semester}}</td>
-                  <td><button @click="deleteStudentForm(index)" style="cursor:pointer">
+                  <td class="delete"><button class="btn btn-success" v-on:click="deleteStudentForm(index)" style="cursor:pointer">
                   X
                   </button>
                   </td>
@@ -52,6 +55,7 @@ export default {
   el: '.container',
   data: function() {
 return {
+    semester_select: null,
     tableHeader: [
       '#',
       'Name',
@@ -93,39 +97,61 @@ return {
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-/* .card{
+.card{
   border: 2px solid #000000;
   border-radius: 3px;
   background-color: #fff;
-} */
-table {
-  border: 2px solid #42b983;
+}
+.attendee_form input{
+}
+.attendee_form_input select{
+}
+.attendee_form button{
+  background-color: #005ea8;
+}
+/* table {
+  border: 2px solid #005ea8;
   border-radius: 3px;
   background-color: #fff;
-}
-.attendees_list thead {
-  /* border: 2px solid #ffffff;
-  background-color: #42b983;
-  color: rgba(255,255,255,0.66); */
-  background-color: #42b983;
-  color: rgba(255,255,255,0.66);
+} */
+/* .attendees_list thead {
+  background-color: #005ea8;
+  color: rgba(247, 247, 247, 0.66);
   cursor: pointer;
   -webkit-user-select: none;
   -moz-user-select: none;
   -ms-user-select: none;
   user-select: none;
-}
-.attendees_list thead td {
-  padding: 0px 0 0 10px;  
-}
-.attendees_list tbody tbody td {
-  padding: 5px 0 0 10px;
-}
-.ui-table-header-cell center{
-  text-align:left
-}
-td {
+} */
+.attendees_list thead td{
+  background-color: #005ea8;
+  color: rgba(247, 247, 247, 0.66);
+  cursor: pointer;
   min-width: 150px;
-  padding: 10px 20px;
+  /* border: 2px solid #ffffff; */
+  border-radius: 3px;
+  }
+.ui-table-header-cell center{
+  text-align:center
+}
+.attendees_list .attendee_rows td {
+  border: 2px solid #ffffff;
+  border-radius: 2px;
+  min-width: 150px;
+  /* padding: 5px 0 0 10px; */
+  background-color: #f3f3f3;
+}
+.attendees_list .attendee_rows button{
+  /* border: 2px solid #ffffff;
+  border-radius: 2px; */
+  /* padding: 5px 0 0 10px; */
+  background-color: #005ea8;
+  color: #f3f3f3;
+}
+.attendees_list .attendee_rows .delete{
+  /* border: 2px solid #ffffff;
+  border-radius: 2px; */
+  /* padding: 5px 0 0 10px; */
+  background-color: #f3f3f3;
 }
 </style>
