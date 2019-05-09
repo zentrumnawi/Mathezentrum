f9a444d43bfe8f3b0025d3a2388ca7fd9547a685
Michael Gecht <gecht.m@gmail.com>
2019-05-09 22:02:47 +0200

    Fix json2csv conversion

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -5,99 +5,80 @@
         <h3 class="headline mb-0">Teilnehmerliste</h3>
       </div>
     </v-card-title>
-    <v-data-table
-          :headers="headers"
-          :items="values"
-          class="elevation-1"
-        >
-          <template v-slot:items="props">
-            <td>{{ props.item.id }}</td>
-            <td class="text-xs-center">{{ props.item.date }}</td>
-            <td class="text-xs-center">{{ props.item.start }}</td>
-            <td class="text-xs-center">{{ props.item.end }}</td>
-            <td class="text-xs-center">{{ props.item.courses.join(', ') }}</td>
-            <td class="text-xs-center">{{ props.item.semester }}</td>
-            <td class="text-xs-center">{{ props.item.faculty }}</td>
-          </template>
-        </v-data-table>
-      <v-btn class="success" @click="downloadURL">
-        <!-- <json2csv
-          :data="this.$store.state.attendees"
-          :fields="headers.text"
-        > -->
-        <a 
-          :download="downloadName"
-          :href="downloadURL"
-          :disabled="this.$store.state.attendees.length === 0">
-          Download
-          <!-- <slot>
-            <i class="fa fa-file-excel-o" aria-hidden="true"></i>
-          </slot> -->
-        </a>
-      <!-- </json2csv> -->
+    <v-data-table :headers="headers" :items="values" class="elevation-1">
+      <template v-slot:items="props">
+        <td>{{ props.item.id }}</td>
+        <td class="text-xs-center">{{ props.item.date }}</td>
+        <td class="text-xs-center">{{ props.item.start }}</td>
+        <td class="text-xs-center">{{ props.item.end }}</td>
+        <td class="text-xs-center">{{ props.item.courses.join(', ') }}</td>
+        <td class="text-xs-center">{{ props.item.semester }}</td>
+        <td class="text-xs-center">{{ props.item.faculty }}</td>
+      </template>
+    </v-data-table>
+    <v-btn class="success" :href="downloadURL" :disabled="this.$store.state.attendees.length === 0">
+      Download
     </v-btn>
-    </div>
+  </div>
 </template>
 
 <script>
-// import json2csv from 'json2csv';
-const json2csv = require('json2csv').parse
+import { parse } from "json2csv";
+
 export default {
   data: function() {
     return {
       headers: [
         {
-          text: 'ID',
-          align: 'left',
+          text: "ID",
+          align: "left",
           sortable: true,
-          value: 'id'
+          value: "id"
         },
-        { text: 'Datum', value: 'date' },
-        { text: 'Startzeit', value: 'start' },
-        { text: 'Endzeit', value: 'end' },
-        { text: 'Kurse', value: 'courses' },
-        { text: 'Semester', value: 'semester' },
-        { text: 'Studiengang', value: 'faculty'}
-      ],
-        // delimiter: ";",
-        // downloadName: {
-        //     default: 'export.csv'
-        // }
+        { text: "Datum", value: "date" },
+        { text: "Startzeit", value: "start" },
+        { text: "Endzeit", value: "end" },
+        { text: "Kurse", value: "courses" },
+        { text: "Semester", value: "semester" },
+        { text: "Studiengang", value: "faculty" }
+      ]
+      // delimiter: ";",
+      // downloadName: {
+      //     default: 'export.csv'
+      // }
       // data: this.$store.state.attendees,
       // fields: this.headers.text
+    };
+  },
+  props: {
+    downloadName: {
+      default: "export.csv"
+    },
+    delimiter: {
+      default: ";"
     }
   },
-      props: {
-        downloadName: {
-            default: 'export.csv'
-        },
-        delimiter: {
-          default: ';'
-        }
-    },
-
-  // components: {
-  //   json2csv: Json2Csv,
-  // },
   computed: {
-    values () {
-      return this.$store.state.attendees
+    values() {
+      return this.$store.state.attendees;
     },
-    data () {
-      return this.$store.state.attendees
+    data() {
+      return this.$store.state.attendees;
     },
-    fields () {
-      return this.headers.text
+    fields() {
+      return this.headers.map(item => item.value);
+    },
+    csv() {
+      const opts = { ...this.fields };
+
+      const csv = parse(this.data, opts);
+      return csv;
     },
     downloadURL() {
-      return this.$store.state.attendees.length > 0 ? "data:text/csv," + encodeURIComponent(json2csv({data:this.data, fields:this.fields})) : 'javascript:void(0);';
+      return this.$store.state.attendees.length > 0
+        ? "data:text/csv," + encodeURIComponent(this.csv)
+        : "javascript:void(0);";
     }
   }
-}
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.v-text-field{
-}
-</style>
