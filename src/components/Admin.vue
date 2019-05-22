f1dacd0fd9a710f492c26995b3317abd082662f0
unknown <github@sheepyhollow.de>
2019-05-22 15:25:49 +0200

    fixed export

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -17,18 +17,15 @@
         <td class="text-xs-left">{{ props.item.courses.join(', ') }}</td>
       </template>
     </v-data-table>
-    <!--v-btn class="success" :download="downloadName" :href="downloadURL" :disabled="this.$store.state.attendees.length === 0">
-      Download
-    </v-btn-->
     <v-btn class="success" :download="downloadName" :href="downloadURL" :disabled="this.$store.state.attendees.length === 0">
       Download
     </v-btn>
-    
   </div>
+
 </template>
 
 <script>
-//import { parse } from "json2csv";
+import { parse } from "json2csv";
 import { format, addMinutes, differenceInMinutes } from "date-fns";
 
 export default {
@@ -67,67 +64,43 @@ export default {
       default: ''
     }
   },
+  methods: {
+
+    format(element) {
+      const differenceMinutes = differenceInMinutes(element.end, element.start)
+      const differenceDate = addMinutes(new Date(0), differenceMinutes)
+
+      return {
+        presence: format(differenceDate,'HH:mm'),
+        date: format(element.start, 'MM.DD.YYYY'),
+        start: format(element.start,'HH:mm'),
+        end: format(element.end,'HH:mm')
+      };
+    },
+  },
   computed: {
     
     values() {
 
-        this.$store.state.attendees.forEach(element => {
-        element.presence = format(addMinutes(new Date(0),differenceInMinutes(element.end, element.start)),'HH:mm');
-        element.date = format(element.start, 'MM.DD.YYYY');
-        element.start = format(element.start,'HH:mm');
-        element.end = format(element.end,'HH:mm');
-      });
+      const newvalues = this.$store.state.attendees;
 
-    return this.$store.state.attendees;
+      return newvalues.map(element => ({ ...element, ...this.format(element) }));
     },
-//    data() {
-//          return this.$store.state.attendees;
-//    },
     fields() {
-      return this.headers.map(item => item.text);
+      return this.headers.map(item => ({
+        label: item.text,
+        value: item.value
+      }));
     },
-//    csv() {
-//      const opts = {...this.fields, delimiter: this.delimiter, quote: this.quote };
-//
-//      const csv = parse(this.values, opts);
-//
-//      return csv;
- //   },
-    mancsv() {
-
-      let output = "";
-     
-      this.fields.forEach(element => {
-        output += element;
-        output += this.delimiter; 
-        
-      } );
-
-      output += '\n';
-
-      this.$store.state.attendees.forEach(element => {
-        output += element.date + this.delimiter;
-        output += element.id + this.delimiter;
-        output += element.start + this.delimiter;
-        output += element.end + this.delimiter;
-        output += element.presence + this.delimiter;
-        output += element.faculty + this.delimiter;
-        output += element.semester + this.delimiter;
-        output += element.courses.join(', ') + this.delimiter;
-        output += '\n';
-      });
-
-      return output;
-    },
-
-//    downloadURL() {
-//     return this.$store.state.attendees.length > 0
-//        ? "data:text/csv," + encodeURIComponent(this.csv)
-//        : "javascript:void(0);";
-//    },
+    csv() {
+      const opts = {fields: this.fields, delimiter: this.delimiter, quote: this.quote };    
+      const csv = parse(this.values, opts);
+      
+      return csv;
+    },  
     downloadURL() {
       return this.$store.state.attendees.length > 0
-        ? "data:text/csv," + encodeURIComponent(this.mancsv)
+        ? "data:text/csv," + encodeURIComponent(this.csv)
         : "javascript:void(0);";
     }
   }
